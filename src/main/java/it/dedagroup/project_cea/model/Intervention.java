9ba85cd65914b2792b56041ab1a19c91ef00168b
giulioniale <giulioni92@gmail.com>
2023-10-16 18:14:06 +0200

    Fix relazioni: Secretary e Technician (rimossa) Technician e Intervention (aggiunta) Secretary e Intervention (aggiunta)
    Ho rimosso ed agiunto alcuni metodi in TechnicianServiceDef.

--- src/main/java/it/dedagroup/project_cea/model/Intervention.java
+++ src/main/java/it/dedagroup/project_cea/model/Intervention.java
@@ -2,6 +2,7 @@ package it.dedagroup.project_cea.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -18,19 +19,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Intervention {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@Column(nullable = false)
 	private LocalDate date;
-	@ManyToOne(fetch = FetchType.LAZY)
-	private Technician technician;
-	@ManyToOne(fetch = FetchType.LAZY)
-	private Apartment apartment;
 	@Column(nullable = false)
 	private boolean isAvailable;
 	@Column(nullable = false)
 	private TypeOfIntervention type;
 	@Column(nullable = false)
 	private StatusIntervention status;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Technician technician;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Apartment apartment;
+	@ManyToOne(fetch = FetchType.LAZY,  cascade = {CascadeType.PERSIST,  CascadeType.MERGE})
+	Secretary secretary;
 }
--- src/main/java/it/dedagroup/project_cea/service/impl/TechnicianServiceImpl.java
+++ src/main/java/it/dedagroup/project_cea/service/impl/TechnicianServiceImpl.java
@@ -8,14 +8,19 @@ import it.dedagroup.project_cea.dto.request.TechnicianRequest;
 import it.dedagroup.project_cea.model.Intervention;
 import it.dedagroup.project_cea.model.Secretary;
 import it.dedagroup.project_cea.model.Technician;
-import it.dedagroup.project_cea.model.User;
 import it.dedagroup.project_cea.service.def.TechnicianServiceDef;
 
 @Service
 public class TechnicianServiceImpl implements TechnicianServiceDef{
 
 	@Override
-	public Technician addTechnician(User user, List<Intervention> interventions, Secretary secretary) {
+	public void save(Technician t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Technician update(Technician t) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -27,25 +32,19 @@ public class TechnicianServiceImpl implements TechnicianServiceDef{
 	}
 
 	@Override
-	public void save(Technician t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<Technician> findBySecretary(long idSecretary) {
+	public Technician findByIntervention(long idIntervention) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Technician> findByIntervention(long idIntervention) {
+	public Technician findById(long idTechnician) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Technician> findById(long idTechnician) {
+	public Technician findByUsername(String username) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -55,5 +54,12 @@ public class TechnicianServiceImpl implements TechnicianServiceDef{
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public List<Technician> findFree() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
