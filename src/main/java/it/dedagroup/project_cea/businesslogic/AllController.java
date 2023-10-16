c61e00b78657afbbdc454b81994aed9c9d6274d9
SCFVLR97A <SCFVLR97A@G92FRV3.nco.inet>
2023-10-16 18:57:31 +0200

    -Implementazione funzionalità di registrazione per tutti gli utenti

--- src/main/java/it/dedagroup/project_cea/businesslogic/AllController.java
+++ src/main/java/it/dedagroup/project_cea/businesslogic/AllController.java
@@ -1,21 +1,35 @@
 package it.dedagroup.project_cea.businesslogic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.dedagroup.project_cea.dto.request.LoginDTORequest;
+import it.dedagroup.project_cea.dto.request.RegisterUserDto;
 import it.dedagroup.project_cea.dto.response.LoginDTOResponse;
+import it.dedagroup.project_cea.facade.AllFacade;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/all")
 public class AllController {
 	
+	@Autowired
+	private AllFacade allFacade;
+	
 	public ResponseEntity<LoginDTOResponse> login(@Valid @RequestBody LoginDTORequest request){
 		return null;
 	}
+	
+	@PostMapping("/register/user")
+	public ResponseEntity<String> registerAccount(@Valid @RequestBody RegisterUserDto request){
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(allFacade.registerUser(request));
+	}
+
 	
 
 }
---
+++ src/main/java/it/dedagroup/project_cea/service/def/UserServiceDef.java
@@ -0,0 +1,9 @@
+package it.dedagroup.project_cea.service.def;
+
+import it.dedagroup.project_cea.model.User;
+
+public interface UserServiceDef {
+	
+	void addUser(User user);
+
+}
