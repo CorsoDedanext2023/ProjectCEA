c43c888a58e16d029993bf394a7232211212606e
CMPMKR01R <CMPMKR01R@FGSQTT3.nco.inet>
2023-10-18 18:06:34 +0200

    Implementati i metodi find all'interno del CustomerFacade
    CustomerFacade:
    - metodi implementati:
    	- findAllCustomerByNameAndSurname
    	- findCustomerByApartments_Id
    
    AddApartmentDto:
    - validation inserite per ogni attributi
    
    ApartmentDto:
    - attributi implementati + lombok
    
    ApartmentMapper:
    - implementato il metodo toDto

--- src/main/java/it/dedagroup/project_cea/dto/response/ApartmentDto.java
+++ src/main/java/it/dedagroup/project_cea/dto/response/ApartmentDto.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 //TODO Implementare i vari attributi di Apartment
-//TODO Implementare le validation di Apartment
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApartmentDto {
 	private long id;
 	private int unitNumber;
--- src/main/java/it/dedagroup/project_cea/facade/CustomerFacade.java
+++ src/main/java/it/dedagroup/project_cea/facade/CustomerFacade.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import it.dedagroup.project_cea.dto.request.AddCustomerDto;
 import it.dedagroup.project_cea.dto.response.CustomerDto;
 import it.dedagroup.project_cea.exception.model.NotValidDataException;
+import it.dedagroup.project_cea.mapper.CustomerMapper;
 import it.dedagroup.project_cea.model.Bill;
 import it.dedagroup.project_cea.model.Customer;
 import it.dedagroup.project_cea.model.Intervention;
@@ -18,6 +19,8 @@ import it.dedagroup.project_cea.service.def.CustomerServiceDef;
 public class CustomerFacade {
 	@Autowired
 	CustomerServiceDef customerServiceDef;
+	@Autowired
+	CustomerMapper customerMapper;
 	//TODO CustomerFacade da implementare + controlli
 	public void saveCustomer(AddCustomerDto request) {
 		try {
@@ -31,36 +34,37 @@ public class CustomerFacade {
 			customerAdd.setTaxCode(request.getTaxCode());
 			customerServiceDef.saveCustomer(customerAdd);
 		}
-		throw new NotValidDataException("Error in insert a new customer");
+		throw new NotValidDataException("Error existing user with username: "+request.getUsername());
 	}
 	public void modifyCustomer(Customer customer){
 	}
 	
-	public void deleteCustomer(long customer_id){
+	public void deleteCustomer(long id_customer){
 
 	}
 	
-	public Intervention bookIntervention(long user_id, LocalDate interventionDate) {
+	public Intervention bookIntervention(long id_user, LocalDate interventionDate) {
 		return null;
 	}
 
-	public List<Bill> getBills(long user_id) {
+	public List<Bill> getBills(long id_user) {
 		return null;
 	}
 
-	public Bill payBill(long bill_id, LocalDate paymentDate) {
+	public Bill payBill(long id_bill, LocalDate paymentDate) {
 		return null;
 	}
 
-	public Scan meterScan(long apartment_id, Bill lastBill) {
+	public Scan meterScan(long id_apartment, Bill lastBill) {
 		return null;
 	}
 	
-	public CustomerDto findCustomerById(long customer_id){
-		return null;
+	public CustomerDto findCustomerById(long id_customer){
+		if(id_customer < 0)throw new NotValidDataException("Error insert a valid customer id: "+id_customer);
+		return customerMapper.toDto(customerServiceDef.findCustomerById(id_customer));
 	}
 	public List<CustomerDto> findAllCustomer(){
-		return null;
+		return customerMapper.toListDto(customerServiceDef.findAllCustomer());
 	}
 	public CustomerDto findCustomerByUsernameAndPassword(String username, String password){
 		return null;
@@ -72,9 +76,14 @@ public class CustomerFacade {
 		return null;
 	}
 	public List<CustomerDto> findAllCustomerByNameAndSurname(String name, String surname){
-		return null;
+		if (name == null || name.isEmpty())throw new NotValidDataException("Insert a value on field name");
+		if (surname == null || surname.isEmpty())throw new NotValidDataException("Insert a value on field surname");
+		if (!name.matches("^[\\p{L} '-]+$"))throw new NotValidDataException("Insert a valid name: "+name);
+		if (!surname.matches("^[\\p{L} '-]+$"))throw new NotValidDataException("Insert a valid surname: "+surname);
+		return customerMapper.toListDto(customerServiceDef.findAllCustomerByNameAndSurname(name, surname));
 	}
-	public CustomerDto findCustomerByApartments_Id(long apartment_id){
-		return null;
+	public CustomerDto findCustomerByApartments_Id(long id_apartment){
+		if (id_apartment < 0)throw new NotValidDataException("Error insert a valid apartment id: "+id_apartment);
+		return customerMapper.toDto(customerServiceDef.findCustomerByApartments_Id(id_apartment));
 	}
 }
