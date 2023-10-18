4191def4dba795f981b75d30c7416fb2ce3edb8e
giulioniale <giulioni92@gmail.com>
2023-10-18 16:31:46 +0200

    Rimozione implementazioni Intervention errate nelle varie classi

--- src/main/java/it/dedagroup/project_cea/service/def/InterventionServiceDef.java
+++ src/main/java/it/dedagroup/project_cea/service/def/InterventionServiceDef.java
@@ -1,15 +1,6 @@
 package it.dedagroup.project_cea.service.def;
 
-import java.util.List;
-
-import it.dedagroup.project_cea.model.Intervention;
-import it.dedagroup.project_cea.model.TypeOfIntervention;
 
 public interface InterventionServiceDef {
-	
-	public Intervention findById(Long idIntervention);
-	public List<Intervention> findAll();
-	public List<Intervention>findAllByStatus(String status);
-	public List<Intervention> findAllByType(TypeOfIntervention type);
 	
 }
