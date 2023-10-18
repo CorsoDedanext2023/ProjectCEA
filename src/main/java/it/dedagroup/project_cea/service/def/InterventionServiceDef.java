0a46621d0f6392e9c8bc529c052d045e5e8a3a1d
giulioniale <giulioni92@gmail.com>
2023-10-18 16:17:37 +0200

    ApartmentRepository: -rinominato metodo findApartmentByScanID() in findApartmentByScans_ID()
    InterventionRepository:
    -aggiunto metodo findByStatus
    
    InterventionServiceDef:
    aggiunto finAll
    aggiunto findByID
    aggiunto findAllByStatus
    
    InterventionServiceImpl:
    -aggiunti metodi mancanti
    
    Fixato data.sql

--- src/main/java/it/dedagroup/project_cea/service/def/InterventionServiceDef.java
+++ src/main/java/it/dedagroup/project_cea/service/def/InterventionServiceDef.java
@@ -7,6 +7,9 @@ import it.dedagroup.project_cea.model.TypeOfIntervention;
 
 public interface InterventionServiceDef {
 	
-	public List<Intervention> findAllByType(TypeOfIntervention type);
 	public Intervention findById(Long idIntervention);
+	public List<Intervention> findAll();
+	public List<Intervention>findAllByStatus(String status);
+	public List<Intervention> findAllByType(TypeOfIntervention type);
+	
 }
