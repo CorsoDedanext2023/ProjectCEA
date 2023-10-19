1f73e9edddd47758b7ee285e2273525e7cf0e1f8
CSLMHL99D <CSLMHL99D@6JGFRV3.nco.inet>
2023-10-19 10:38:19 +0200

    format file

--- src/main/java/it/dedagroup/project_cea/facade/CustomerFacade.java
+++ src/main/java/it/dedagroup/project_cea/facade/CustomerFacade.java
@@ -21,15 +21,15 @@ import it.dedagroup.project_cea.service.def.CustomerServiceDef;
 
 @Service
 public class CustomerFacade {
-	
+
 	@Autowired
 	CustomerServiceDef customerServiceDef;
 	@Autowired
 	CustomerMapper customerMapper;
-	//TODO CustomerFacade da implementare + controlli
+	// TODO CustomerFacade da implementare + controlli
 	@Autowired
 	CustomerMapper CustomerMapper;
-	
+
 	public void saveCustomer(AddCustomerDto request) {
 		try {
 			customerServiceDef.findCustomerByUsername(request.getUsername());
@@ -42,51 +42,48 @@ public class CustomerFacade {
 			customerAdd.setTaxCode(request.getTaxCode());
 			customerServiceDef.saveCustomer(customerAdd);
 		}
-		throw new NotValidDataException("Error existing user with username: "+request.getUsername());
+		throw new NotValidDataException("Error existing user with username: " + request.getUsername());
 	}
-	
-	public void modifyCustomer(EditCustomerDto request){
-		try {
-			Customer customer=customerServiceDef.findCustomerById(request.getId());
-			if(!customer.getName().equals(request.getName())) {
-				customer.setName(request.getName());
-			}
-			if(!customer.getSurname().equals(request.getSurname())) {
-				customer.setSurname(request.getSurname());
-			}
-			if(customerServiceDef.findCustomerByUsername(request.getUsername())!=null) {
-				throw new NotValidDataException("Existing username!");
-			}else if(!customer.getUsername().equals(request.getUsername())) {
-				customer.setUsername(request.getUsername());
-			}
-			if(!customer.getPassword().equals(request.getOldPassword())) {
-				throw new NotValidDataException("Wrong password!");
-			}else if(!request.getNewPassword().equals(request.getRepeatNewPassword())) {
-				throw new NotValidDataException("Repeated password doesn't match!");
-			}else if(request.getNewPassword().equals(request.getRepeatNewPassword())) {
-				customer.setPassword(request.getNewPassword());
-			}
-			if(!customer.getTaxCode().equals(request.getTaxCode())) {
-				customer.setTaxCode(request.getTaxCode());
-			}
-			customerServiceDef.modifyCustomer(customer);
-		}catch(NotValidDataException e) {
-			e.getMessage();
+
+	public void modifyCustomer(EditCustomerDto request) {
+		Customer customer = customerServiceDef.findCustomerById(request.getId());
+		if (!customer.getName().equals(request.getName())) {
+			customer.setName(request.getName());
 		}
+		if (!customer.getSurname().equals(request.getSurname())) {
+			customer.setSurname(request.getSurname());
+		}
+		if (customerServiceDef.findCustomerByUsername(request.getUsername()) != null) {
+			throw new NotValidDataException("Existing username!");
+		} else if (!customer.getUsername().equals(request.getUsername())) {
+			customer.setUsername(request.getUsername());
+		}
+		if (!customer.getPassword().equals(request.getOldPassword())) {
+			throw new NotValidDataException("Wrong password!");
+		} else if (!request.getNewPassword().equals(request.getRepeatNewPassword())) {
+			throw new NotValidDataException("Repeated password doesn't match!");
+		} else if (request.getNewPassword().equals(request.getRepeatNewPassword())) {
+			customer.setPassword(request.getNewPassword());
+		}
+		if (!customer.getTaxCode().equals(request.getTaxCode())) {
+			customer.setTaxCode(request.getTaxCode());
+		}
+		customerServiceDef.modifyCustomer(customer);
 	}
-	
-	public void deleteCustomer(long id_customer){
+
+	public void deleteCustomer(long id_customer) {
 		customerServiceDef.deleteCustomer(id_customer);
 	}
-	
+
 	public Intervention bookIntervention(BookInterventionDto request) {
-		return customerServiceDef.bookIntervention(request.getIdCustomer(), request.getIdApartment(), request.getInterventionDate());
+		return customerServiceDef.bookIntervention(request.getIdCustomer(), request.getIdApartment(),
+				request.getInterventionDate());
 	}
-	
+
 	public List<Bill> getBills(long id_user) {
 		return null;
 	}
-	
+
 	public Bill payBill(PayBillDto request) {
 		return customerServiceDef.payBill(request.getIdBill(), request.getPaymentDate());
 	}
@@ -94,46 +91,52 @@ public class CustomerFacade {
 	public Scan meterScan(MeterScanDto request) {
 		return customerServiceDef.meterScan(request.getIdApartment(), request.getMcLiter());
 	}
-	
-	public CustomerDto findCustomerById(long id_customer){
-		if(id_customer < 0)throw new NotValidDataException("Error insert a valid customer id: "+id_customer);
+
+	public CustomerDto findCustomerById(long id_customer) {
+		if (id_customer < 0)
+			throw new NotValidDataException("Error insert a valid customer id: " + id_customer);
 		return customerMapper.toDto(customerServiceDef.findCustomerById(id_customer));
 	}
-	
-	public List<CustomerDto> findAllCustomer(){
+
+	public List<CustomerDto> findAllCustomer() {
 		return customerMapper.toListDto(customerServiceDef.findAllCustomer());
 	}
-	
-	public CustomerDto findCustomerByUsernameAndPassword(String username, String password){
+
+	public CustomerDto findCustomerByUsernameAndPassword(String username, String password) {
 		return null;
 	}
-	
-	public CustomerDto findCustomerByUsername(String username){
+
+	public CustomerDto findCustomerByUsername(String username) {
 		String regexUsername = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
-		if (username==null || username.trim().isEmpty() || !username.matches(regexUsername)) {
+		if (username == null || username.trim().isEmpty() || !username.matches(regexUsername)) {
 			throw new NotValidDataException("Error, insert a valid username");
 		}
 		return CustomerMapper.toDto(customerServiceDef.findCustomerByUsername(regexUsername));
 	}
-	
-	public CustomerDto findCustomerByTax_Code(String taxCode){
+
+	public CustomerDto findCustomerByTax_Code(String taxCode) {
 		String regexParametri = "^[A-Z]{6}\\d{2}[A-Z]\\d{2}[A-Z]\\d{3}[A-Z]$";
 		if (!taxCode.matches(regexParametri)) {
 			throw new NotValidDataException("Error, insert a valid tax code");
 		}
 		return CustomerMapper.toDto(customerServiceDef.findCustomerByTaxCode(taxCode));
 	}
-	
-	public List<CustomerDto> findAllCustomerByNameAndSurname(String name, String surname){
-		if (name == null || name.isEmpty())throw new NotValidDataException("Insert a value on field name");
-		if (surname == null || surname.isEmpty())throw new NotValidDataException("Insert a value on field surname");
-		if (!name.matches("^[\\p{L} '-]+$"))throw new NotValidDataException("Insert a valid name: "+name);
-		if (!surname.matches("^[\\p{L} '-]+$"))throw new NotValidDataException("Insert a valid surname: "+surname);
+
+	public List<CustomerDto> findAllCustomerByNameAndSurname(String name, String surname) {
+		if (name == null || name.isEmpty())
+			throw new NotValidDataException("Insert a value on field name");
+		if (surname == null || surname.isEmpty())
+			throw new NotValidDataException("Insert a value on field surname");
+		if (!name.matches("^[\\p{L} '-]+$"))
+			throw new NotValidDataException("Insert a valid name: " + name);
+		if (!surname.matches("^[\\p{L} '-]+$"))
+			throw new NotValidDataException("Insert a valid surname: " + surname);
 		return customerMapper.toListDto(customerServiceDef.findAllCustomerByNameAndSurname(name, surname));
 	}
-	
-	public CustomerDto findCustomerByApartments_Id(long id_apartment){
-		if (id_apartment < 0)throw new NotValidDataException("Error insert a valid apartment id: "+id_apartment);
+
+	public CustomerDto findCustomerByApartments_Id(long id_apartment) {
+		if (id_apartment < 0)
+			throw new NotValidDataException("Error insert a valid apartment id: " + id_apartment);
 		return customerMapper.toDto(customerServiceDef.findCustomerByApartments_Id(id_apartment));
 	}
 }
