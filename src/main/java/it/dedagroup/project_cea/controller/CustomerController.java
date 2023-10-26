2b0cd24391ec5d25a1435ec97101c762a7dc97f1
VLPGMR02B <VLPGMR02B@8N8FRV3.nco.inet>
2023-10-26 09:09:03 +0200

    COMMIT DI UPDATE -GIANMARCO VOLPE-

--- src/main/java/it/dedagroup/project_cea/controller/CustomerController.java
+++ src/main/java/it/dedagroup/project_cea/controller/CustomerController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 import it.dedagroup.project_cea.dto.request.LoginDTORequest;
 import it.dedagroup.project_cea.dto.request.CustomerNameSurnameDtoRequest;
 import it.dedagroup.project_cea.dto.response.CustomerDtoResponse;
@@ -28,6 +30,7 @@ import jakarta.validation.Valid;
 import static it.dedagroup.project_cea.util.UtilPath.*;
 
 @RestController
+@Tag(name="metodi del Customer", description="")
 @RequestMapping("/technician")
 public class CustomerController {
 	
